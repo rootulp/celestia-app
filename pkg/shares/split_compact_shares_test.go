75c52875469d686719e0fd0035d287089fce06ab
Rootul Patel <rootulp@gmail.com>
2023-01-27 14:08:36 -0500

    test: TestWriteAfterExportV2

--- pkg/shares/split_compact_shares_test.go
+++ pkg/shares/split_compact_shares_test.go
@@ -118,3 +118,67 @@ func TestExport(t *testing.T) {
 		})
 	}
 }
+
+func TestWriteAfterExportV2(t *testing.T) {
+	type testCase struct {
+		name    string
+		txs     []coretypes.Tx
+		wantLen int
+	}
+	testCases := []testCase{
+		{
+			name:    "one tx that occupies exactly one share",
+			txs:     []coretypes.Tx{generateTx(1)},
+			wantLen: 1,
+		},
+		{
+			name:    "one tx that occupies exactly two shares",
+			txs:     []coretypes.Tx{generateTx(2)},
+			wantLen: 2,
+		},
+		{
+			name:    "one tx that occupies exactly three shares",
+			txs:     []coretypes.Tx{generateTx(3)},
+			wantLen: 3,
+		},
+		{
+			name: "two txs that occupy exactly two shares",
+			txs: []coretypes.Tx{
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.FirstCompactShareContentSize)),
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.ContinuationCompactShareContentSize)),
+			},
+			wantLen: 2,
+		},
+		{
+			name: "three txs that occupy exactly three shares",
+			txs: []coretypes.Tx{
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.FirstCompactShareContentSize)),
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.ContinuationCompactShareContentSize)),
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.ContinuationCompactShareContentSize)),
+			},
+			wantLen: 3,
+		},
+		{
+			name: "four txs that occupy three full shares and one partial share",
+			txs: []coretypes.Tx{
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.FirstCompactShareContentSize)),
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.ContinuationCompactShareContentSize)),
+				bytes.Repeat([]byte{0xf}, rawTxSize(appconsts.ContinuationCompactShareContentSize)),
+				[]byte{0xf},
+			},
+			wantLen: 4,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			css := NewCompactShareSplitter(appconsts.TxNamespaceID, appconsts.ShareVersionZero)
+
+			for _, tx := range tc.txs {
+				css.WriteTx(tx)
+			}
+
+			assert.Equal(t, tc.wantLen, css.Count())
+			assert.Equal(t, tc.wantLen, len(css.Export()))
+		})
+	}
+}
