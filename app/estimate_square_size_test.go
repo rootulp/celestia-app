6904d888b4d59797efb7d76e1c7458b75f9b198a
Rootul Patel <rootulp@gmail.com>
2023-03-22 12:24:14 +0100

    fix estimate tx shares size

--- app/estimate_square_size_test.go
+++ app/estimate_square_size_test.go
@@ -158,7 +158,7 @@ func Test_estimateTxSharesUsed(t *testing.T) {
 		{"one tx", generateNormalTxs(1), 1},             // 1 tx is approximately 316 bytes which fits in 1 share
 		{"two txs", generateNormalTxs(2), 2},            // 2 txs is approximately 632 bytes which fits in 2 shares
 		{"ten txs", generateNormalTxs(10), 7},           // 10 txs is approximately 3160 bytes which fits in 7 shares
-		{"one hundred txs", generateNormalTxs(100), 67}, // 100 txs is approximately 31600 bytes which fits in 67 share
+		{"one hundred txs", generateNormalTxs(100), 68}, // 100 txs is approximately 31600 bytes which fits in 68 share
 	}
 	for _, tc := range testCases {
 		got := estimateTxSharesUsed(tc.txs)
