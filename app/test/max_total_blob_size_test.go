30d029c6a4c7a5752c039f93f68d1b38c9561e29
Rootul Patel <rootulp@gmail.com>
2023-08-10 14:53:47 -0400

    fuzz blob too large

--- app/test/max_total_blob_size_test.go
+++ app/test/max_total_blob_size_test.go
@@ -1,12 +1,13 @@
 package app_test
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"testing"
 
-	"github.com/celestiaorg/celestia-app/app"
-	"github.com/celestiaorg/celestia-app/app/encoding"
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
+	appns "github.com/celestiaorg/celestia-app/pkg/namespace"
 	"github.com/celestiaorg/celestia-app/pkg/square"
 	"github.com/celestiaorg/celestia-app/test/util/testfactory"
 	"github.com/celestiaorg/celestia-app/test/util/testnode"
@@ -15,7 +16,6 @@ import (
 	sdk_tx "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 	abci "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	coretypes "github.com/tendermint/tendermint/types"
@@ -27,25 +27,9 @@ const (
 	squareSize = 64
 )
 
-func TestMaxTotalBlobSizeSuite(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping max total blob size suite in short mode.")
-	}
-	suite.Run(t, &MaxTotalBlobSizeSuite{})
-}
-
-type MaxTotalBlobSizeSuite struct {
-	suite.Suite
-
-	ecfg     encoding.Config
-	accounts []string
-	cctx     testnode.Context
-}
-
-func (s *MaxTotalBlobSizeSuite) SetupSuite() {
-	t := s.T()
-
-	s.accounts = testfactory.GenerateAccounts(1)
+func FuzzMaxTotalBlobSizeAnteHandler(f *testing.F) {
+	f.Add([]byte("seed data"))
+	accounts := testfactory.GenerateAccounts(1)
 
 	tmConfig := testnode.DefaultTendermintConfig()
 	tmConfig.Mempool.MaxTxBytes = 10 * mebibyte
@@ -54,67 +38,41 @@ func (s *MaxTotalBlobSizeSuite) SetupSuite() {
 	cParams.Block.MaxBytes = 10 * mebibyte
 
 	cfg := testnode.DefaultConfig().
-		WithAccounts(s.accounts).
+		WithAccounts(accounts).
 		WithTendermintConfig(tmConfig).
 		WithConsensusParams(cParams)
 
-	cctx, _, _ := testnode.NewNetwork(t, cfg)
-	s.cctx = cctx
-	s.ecfg = encoding.MakeConfig(app.ModuleEncodingRegisters...)
+	f.Fuzz(func(t *testing.T, data []byte) {
+		cctx, _, _ := testnode.NewNetwork(t, cfg)
+		require.NoError(t, cctx.WaitForNextBlock())
 
-	require.NoError(t, cctx.WaitForNextBlock())
-}
+		signer := blobtypes.NewKeyringSigner(cctx.Keyring, accounts[0], cctx.ChainID)
 
-// TestSubmitPayForBlob_blobSizes verifies the tx response ABCI code when
-// SubmitPayForBlob is invoked with different blob sizes.
-func (s *MaxTotalBlobSizeSuite) TestSubmitPayForBlob_blobSizes() {
-	t := s.T()
+		ns1 := appns.MustNewV0(bytes.Repeat([]byte{1}, appns.NamespaceVersionZeroIDSize))
+		blob := &tmproto.Blob{
+			NamespaceId:      ns1.ID,
+			NamespaceVersion: uint32(ns1.Version),
+			Data:             data,
+			ShareVersion:     0,
+		}
 
-	type testCase struct {
-		name string
-		blob *tmproto.Blob
-		// want is the expected tx response ABCI code.
-		want uint32
-	}
-	testCases := []testCase{
-		{
-			name: "1 byte blob",
-			blob: mustNewBlob(t, 1),
-			want: abci.CodeTypeOK,
-		},
-		{
-			name: "1 mebibyte blob",
-			blob: mustNewBlob(t, mebibyte),
-			want: abci.CodeTypeOK,
-		},
-		{
-			name: "2 mebibyte blob",
-			blob: mustNewBlob(t, 2*mebibyte),
-			want: types.ErrTotalBlobSizeTooLarge.ABCICode(),
-		},
-	}
+		blobTx := newBlobTx(t, signer, cctx.GRPCClient, blob)
+		res, err := types.BroadcastTx(context.TODO(), cctx.GRPCClient, sdk_tx.BroadcastMode_BROADCAST_MODE_BLOCK, blobTx)
+		require.NoError(t, err)
+		require.NotNil(t, res)
 
-	signer := blobtypes.NewKeyringSigner(s.cctx.Keyring, s.accounts[0], s.cctx.ChainID)
-
-	for _, tc := range testCases {
-		s.Run(tc.name, func() {
-			blobTx := newBlobTx(t, signer, s.cctx.GRPCClient, tc.blob)
-			res, err := types.BroadcastTx(context.TODO(), s.cctx.GRPCClient, sdk_tx.BroadcastMode_BROADCAST_MODE_BLOCK, blobTx)
-			require.NoError(t, err)
-			require.NotNil(t, res)
-			require.Equal(t, tc.want, res.TxResponse.Code, res.TxResponse.Logs)
-
-			sq, err := square.Construct([][]byte{blobTx}, appconsts.LatestVersion, squareSize)
-			if tc.want == abci.CodeTypeOK {
-				// verify that if the tx was accepted, the blob can fit in a square
-				assert.NoError(t, err)
-				assert.False(t, sq.IsEmpty())
-			} else {
-				// verify that if the tx was rejected, the blob can not fit in a square
-				assert.Error(t, err)
-			}
-		})
-	}
+		sq, err := square.Construct([][]byte{blobTx}, appconsts.LatestVersion, squareSize)
+		if res.TxResponse.Code == abci.CodeTypeOK {
+			// verify that if the tx was accepted, the blob can fit in a square
+			assert.NoError(t, err)
+			assert.False(t, sq.IsEmpty())
+			fmt.Printf("verified that tx was accepted and blob can fit in a square\n")
+		} else {
+			// verify that if the tx was rejected, the blob can not fit in a square
+			assert.Error(t, err)
+			fmt.Printf("verified that tx was rejected and blob can not fit in a square\n")
+		}
+	})
 }
 
 func newBlobTx(t *testing.T, signer *blobtypes.KeyringSigner, conn *grpc.ClientConn, blob *tmproto.Blob) coretypes.Tx {
