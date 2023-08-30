ff766b9b3e2cc8f71a3a4023a2edec694ae51d51
Rootul Patel <rootulp@gmail.com>
2023-08-30 16:08:20 -0400

    debug: set bech32 prefix in encoding module

---
+++ app/encoding/init.go
@@ -0,0 +1,24 @@
+package encoding
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const AccountAddressPrefix = "celestia"
+
+var (
+	Bech32PrefixAccAddr  = AccountAddressPrefix
+	Bech32PrefixAccPub   = AccountAddressPrefix + sdk.PrefixPublic
+	Bech32PrefixValAddr  = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixOperator
+	Bech32PrefixValPub   = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixOperator + sdk.PrefixPublic
+	Bech32PrefixConsAddr = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixConsensus
+	Bech32PrefixConsPub  = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
+)
+
+func init() {
+	cfg := sdk.GetConfig()
+	cfg.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	cfg.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	cfg.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+	cfg.Seal()
+}
