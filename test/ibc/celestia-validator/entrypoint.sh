361454b284915415f3de8ec87392820a80cde3d1
Rootul Patel <rootulp@gmail.com>
2025-07-29 00:53:59 -0400

    refactor: delete ibc test

--- test/ibc/celestia-validator/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Copy config files from testing/files/celestia-validator to .tmp directory
-cp -r /testapp_files/* /home/celestia/
-
-exec /bin/celestia-appd start --home /home/celestia --force-no-bbr
